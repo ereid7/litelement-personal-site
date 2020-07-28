2d5418ca97aba9573b107076aeb4e63db0ea88b1
Evan Reid <evan@reids.us>
2020-07-27 20:43:32 -0700

    Create page test file

---
+++ test/components/main/base-page.test.js
@@ -0,0 +1,19 @@
+/* eslint-disable no-unused-expressions */
+import { html, fixture, expect, oneEvent } from '@open-wc/testing';
+import { stub } from 'sinon';
+
+import '../../../src/components/main/base-page.js';
+
+/**
+ * @typedef {import('../../../src/components/main/base-page.js').BasePage} BasePage
+ */
+
+const _aboutmeTemplate = html`<about-me></about-me>`
+
+/**
+ * Helper Methods
+ */
+
+describe('page tests', () => {
+
+});
